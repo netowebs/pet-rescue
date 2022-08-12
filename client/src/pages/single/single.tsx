e5d6d7464e45128c386b5ad7173b71f15a11c8be
oneto <36541462+netowebs@users.noreply.github.com>
2022-08-12 16:22:25 -0300

    12-08-2022--loja

--- client/src/pages/single/single.tsx
+++ client/src/pages/single/single.tsx
@@ -1,6 +1,7 @@
 import { NavBar } from '../../components/navbar/NavBar'
 import { SideBar } from '../../components/sidebar/SideBar'
 import './single.scss'
+import PrintIcon from '@mui/icons-material/Print';
 
 export const Single = () => {
     return(
@@ -13,22 +14,25 @@ export const Single = () => {
                         <div className="inputs">
                             <div className="idCad">
                                 <span>Identificação</span><br />
-                                <input type="number" readOnly/>
+                                <input value={'000384'} type="number" readOnly/>
                             </div>
                             <div className="dtCad">
                                 <span>Data Inclusão</span><br />
-                                <input type="text" />
+                                <input value={'01/01/2022'} type="text" />
                             </div>
                             <div className="dtAlter">
                                 <span>Data Ultima Alteração</span><br />
-                                <input type="text" />
+                                <input value={'01/01/2022'} type="text" />
                             </div>
                         </div>
                         <div className="buttons">
                             <div className="printButton">
-                                <input type="button" value="Salvar" />
+                                <button className='btnPrint'><PrintIcon /></button>
                             </div>
                             <div className="saveButton">
+                                <input type="button" value="Salvar" />
+                            </div>
+                            <div className="cancelButtom">
                                 <input type="button" value="Cancelar" />
                             </div>
                             <div className="deleteButton">
