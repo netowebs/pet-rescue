8b8f32e5f4553a2ef9b27aea829101e9d95964a2
oneto <36541462+netowebs@users.noreply.github.com>
2022-08-17 17:18:38 -0300

    17-08-22

--- client/src/components/datatablePet/single/DtSingle.tsx
+++ client/src/components/datatablePet/single/DtSingle.tsx
@@ -6,77 +6,87 @@ import { pet } from '../../../api/api';
 import './dtsingle.scss'
 
 export const DtSingle = () => {
-    
+
     const params = useParams()
 
-    const [idCad, setIdCad] = useState(Number)
+    const [idCad, setIdCad] = useState(String)
     const [name, setName] = useState(String);
     const [dtRescue, setDtRescue] = useState(String);
     const [species, setSpecies] = useState(String);
-    const [size, setSize] = useState(['MACHO','FEMEA'])
+    const [size, setSize] = useState(String)
 
     const loadPetDetail = async (id: string) => {
         const res = await pet.getPet(id);
-        if(res.success && res.data){
+        if (res.success && res.data) {
             const data = res.data
-            setIdCad(data.id)
-            setDtRescue(data.date_rescue)
+            setIdCad(String("000000" + data.id).slice(-6))
+            setDtRescue(moment(data.date_rescue).format("DD/MM/YYYY"))
             setName(data.name)
             setSpecies(data.species)
             setSize(data.size)
-        }       
+            console.log(size)
+        }
     }
 
-    useEffect(()=>{
-        if(params.Id){
+    useEffect(() => {
+        if (params.Id) {
             loadPetDetail(params.Id)
         }
-    },[])
+    }, [])
 
     return (
         <div className='container-single'>
             <form action="" method="post" className='formDetail'>
                 <div className="topBar">
-                    <label htmlFor="">Código</label>
-                    <input type="text" defaultValue={("000000"+params.Id).slice(-6)}/>
-                    <label htmlFor="">Data Resgate</label>
-                    <input type="text" defaultValue={dtRescue}/>
+                    <div className="topBar-interno">
+                        <div className="topBar-inputs">
+                            <div className="boxId">
+                                <label htmlFor="ipt-id">Código</label><br />
+                                <input className='ipt-id' type="text" defaultValue={idCad} disabled />
+                            </div>
+                            <div className="boxDtCad">
+                                <label htmlFor="ipt-dtRescue">Data Cadastro</label><br />
+                                <input className='ipt-dtRescue' type="text" defaultValue={(dtRescue)} disabled />
+                            </div>
+                            <div className="boxDtRescue">
+                                <label htmlFor="ipt-dtRescue">Data Resgate</label><br />
+                                <input className='ipt-dtRescue' type="text" defaultValue={(dtRescue)} />
+                            </div>
+                        </div>
+                        <div className="topBar-Btn">
+                            <input type="button" value="Salvar" className='btnSalvar' />
+                            <input type="button" value="Cancelar" className='btnCancelar' />
+                            <input type="button" value="Excluir" className='btnExcluir' />
+                        </div>
+                    </div>
                 </div>
                 <div className="middleBar">
-                    <div className="ipt-label">
-                        <label htmlFor="nome">Nome</label>
-                        <input type="text" 
-                            name='nome' 
-                            onChange={(e)=>setName(e.target.value)}
-                            value={name}
-                        />
-                        <label htmlFor="species">Espécie</label>
-                        <input type="text" 
-                            name='species'
-                            onChange={(e)=>setSpecies(e.target.value)}
-                            value={species}
-                        />
-                        <label htmlFor="size">Porte</label>
-                        <select name="size" id="size" defaultValue="DEFAULT">
-                            <option value="DEFAULT" disabled>SELECIONE</option>
-                            <option value="PEQUENO">PEQUENO</option>
-                            <option value="MEDIO">MÉDIO</option>
-                            <option value="GRANDE">GRANDE</option>
-                        </select>
-                        <label htmlFor="sexo">Sexo</label>
-                        <select name="sexo" id="sexo" value={size}>
-                            
-                        </select>
-                        <label htmlFor="age">Idade Aproximada</label>
-                        <input type="number" 
-                            name='age'
-                        />
-                        <label htmlFor="status">Adoção</label>
-                        <select name="status" id="status" defaultValue="DEFAULT">
-                            <option value="DEFAULT" disabled>SELECIONE</option>
-                            <option value="DISPONÍVEL">DISPONÍVEL</option>
-                            <option value="INDISPONÍVEL">INDISPONÍVEL</option>
-                        </select>
+                    <div className="middleBar-interno">
+                            <div className="boxName">
+                                <label htmlFor="ipt-name">Nome</label><br />
+                                <input className='ipt-name' type="text"
+                                    name='nome'
+                                    onChange={(e) => setName(e.target.value)}
+                                    value={name}
+                                />
+                            </div>
+                            <div className="boxSpecies">
+                                <label htmlFor="ipt-species">Espécie</label><br />
+                                <input className='ipt-species' type="text"
+                                    name='species'
+                                    onChange={(e) => setSpecies(e.target.value)}
+                                    value={species}
+                                />
+                            </div>
+                            <div className="boxSize">
+                                <label htmlFor="ipt-size">Porte</label><br />
+                                <select name="ipt-size" id="ipt-size" value={size}>
+                                    <option disabled>Selecione</option>
+                                    <option value="PEQUENO">Pequeno</option>
+                                    <option value="MEDIO">Médio</option>
+                                    <option value="GRANDE">Grande</option>
+                                </select>
+                            </div>
                     </div>
                 </div>
                 <div className="bottonBar">
