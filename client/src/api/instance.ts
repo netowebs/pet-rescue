2db93993d2da406420544bb950213ddbb95ef71b
oneto <36541462+netowebs@users.noreply.github.com>
2023-04-23 15:26:20 -0300

    Update instance.ts

--- client/src/api/instance.ts
+++ client/src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'http://172.17.0.30:3000/',
+    baseURL: 'http://172.17.0.30:3001/',
     headers: {
         'Content-Type': 'application/json',
       },
