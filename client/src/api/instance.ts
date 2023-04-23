b4fcc03cd197810d615b3b0b2f1368c143849798
oneto <36541462+netowebs@users.noreply.github.com>
2023-04-23 18:26:38 -0300

    Update instance.ts

--- client/src/api/instance.ts
+++ client/src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'https://backpetrescue.up.railway.app:3001/',
+    baseURL: 'https://backpetrescue.up.railway.app/',
     headers: {
         'Content-Type': 'application/json',
       },
